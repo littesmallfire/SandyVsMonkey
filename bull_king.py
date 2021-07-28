51e62486be9be0a20dc4f6ede1220e66c818ab75
kangleng <kangleng@gmail.com>
2021-07-28 16:20:05 +0800

    Print output multiple times

--- bull_king.py
+++ bull_king.py
@@ -1,4 +1,8 @@
 def main():
     print("My son is the king of fire")
+    print("My son is the king of fire")
+    print("My son is the king of fire")
+    print("My son is the king of fire")
+
 
 main()
