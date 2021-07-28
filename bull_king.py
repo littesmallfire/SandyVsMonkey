86a5bffb7e0a9e9c4840a7de940104b3ce36c80b
kangleng <kangleng@gmail.com>
2021-07-28 16:23:16 +0800

    Refactor message printing into function

--- bull_king.py
+++ bull_king.py
@@ -1,8 +1,8 @@
 def main():
-    print("My son is the king of fire")
-    print("My son is the king of fire")
-    print("My son is the king of fire")
-    print("My son is the king of fire")
+    print_message("My son is the king of fire", 5)
 
+def print_message(message, number):
+    for i in range(number):
+        print(message)
 
 main()
